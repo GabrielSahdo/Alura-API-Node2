ccd9f12abdde50ece7f1b6e972f081bcbceac29b
sahdo <sahdo@pop-os.localdomain>
2021-12-21 11:14:42 -0400

    aula 2

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const config = require("config");
+const roteador = require("./rotas/fornecedores");
 
 app.use(express.json());
 
-app.listen(config.get('api.porta'), () => console.log("escutando porta 3000..."));
+
+app.use("/api/fornecedores", roteador);
+
+app.listen(config.get("api.porta"), () =>
+    console.log("escutando porta 3000...")
+);
