fe15a08c0eeec4f8708d492aa422837c20d5b25b
sahdo <sahdo@pop-os.localdomain>
2021-12-22 11:08:21 -0400

    aula 3

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,9 +1,32 @@
-const roteador = require('express').Router();
-const TabelaFornecedor = require('./TabelaFornecedor');
+const roteador = require("express").Router();
+const TabelaFornecedor = require("./TabelaFornecedor");
+const Fornecedor = require("./Fornecedor");
 
-roteador.use("/", async (req, res) => {
+roteador.get("/", async (req, res) => {
     const resultados = await TabelaFornecedor.listar();
     res.json(resultados);
+});
+
+roteador.post("/", async (req, res) => {
+    const fornecedor = new Fornecedor(req.body);
+
+    try {
+        await fornecedor.criar();
+        res.status(200).json(fornecedor);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+roteador.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        res.status(200).json(fornecedor);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
 });
 
 module.exports = roteador;
